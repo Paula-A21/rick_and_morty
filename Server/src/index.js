4d46776ec2e3a033036587ba50784e0e43e6abfc
Paula Arce <118852959+Paula-A21@users.noreply.github.com>
2023-06-28 13:01:06 -0300

    agregando el server

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,14 +1,18 @@
 const http = require("http");
+const data = require("./utils/data");
 
 http
-
-    .createServer((req,res) => {
-        const {url} = req;
+    .createServer((req, res) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
+        
+            const {url} = req; 
+            
+            if(url.includes("/rickandmorty/character/")){
 
-            if(url === "/rickandmorty/character"){
-                    
-
+                console.log(url); 
+                const id = Number(url.split("/").at(-1));   
+                console.log(id);
+                res.end(`Muestra el personaje con id: ${id}` );
             }
 
     }).listen(3001, "localhost");
